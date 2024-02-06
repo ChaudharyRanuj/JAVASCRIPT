9219293d4667594a80fd4a118bc92eee775d4a62
RANUJ CHAUDHARY <tridevcomputer01@gmail.com>
2024-02-06 08:18:46 +0530

    added dsa folder

---
+++ DATA_STRUCTURE_ALGORITHM/12-stack.js
@@ -0,0 +1,74 @@
+// /*
+// BIG OF STACKS
+
+// Uses:
+/*
+*Used By Javascript Engine of webbrowser
+
+
+Stack
+
+
+
+
+
+
+*/
+
+
+// INSERTION -  O(1)
+// REMOVAL -  O(1)
+// SEARCHING -  O(N)
+// ACCESS -  O(N)
+// */
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// class Stack {
+//   constructor() {
+//     this.first = null;
+//     this.last = null;
+//     this.size = 0;
+//   }
+
+//   push(val) {
+//     const newNode = new Node(val);
+
+//     if (!this.first) {
+//       this.first = newNode;
+//       this.last = newNode;
+//     } else {
+//       var temp = this.first;
+//       this.first = newNode;
+//       this.first.next = temp;
+//     }
+//     ++this.size;
+//     return this;
+//   }
+
+//   pop() {
+//     if (!this.first) return undefined;
+//     const curr = this.first;
+//     if (this.first === this.last) {
+//       this.last = null;
+//     }
+//     this.first = curr.next;
+//     this.size--;
+//     return curr.val;
+//   }
+// }
+
+// const newStack = new Stack();
+// newStack.push(4);
+// newStack.push(3);
+// newStack.push(2);
+// newStack.push(1);
+// console.log(newStack.pop());
+// console.log(newStack.pop());
+// console.log(newStack);
+
---
+++ DATA_STRUCTURE_ALGORITHM/17-graphs.js
@@ -0,0 +1,48 @@
+/*
+WHAT IS GRAPH?
+
+USES OF GRAPHS
+
+- Social Networks
+- Location Mapping
+- Routing Algorithm
+- Visual Hierarchy
+- File System Optimization
+- recommendation on youtube, netflix, ecommerce website, google ads.
+
+
+UNDIRECTED 
+DIRECTED
+WEIGTED GRAPH
+UNWEIGTED GRAPH
+
+REPRESENTING THE GRAPHS
+ ADJANCY LIST 
+ - can take up less space (in sparse graphs)
+ - faster to iterate over the edges
+ - can be slower to lookup specific edge
+
+ Note:
+ - take less space
+ - 
+
+ ADJANCY MATRIX
+ - takes up more space (in sparse graphs)
+ - slower to iterate over all edges
+ - faster to lookup specific edge
+*/
+
+// UNDIRECTED GRAPH
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+}
+let newGraph  = new Graph()
+newGraph.addVertex('Tokyo')
+newGraph.addVertex('Pattaya')
+newGraph.addVertex('Phuket')
+console.log(newGraph);
