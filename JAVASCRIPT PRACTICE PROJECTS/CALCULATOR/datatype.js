b605e67c25f417cc29ad93cb273d43b14e0bade9
RANUJ CHAUDHARY <tridevcomputer01@gmail.com>
2024-03-06 18:09:39 +0530

    update files double heart click data structure tiktaktoe

--- JAVASCRIPT PRACTICE PROJECTS/CALCULATOR/datatype.js
+++
@@ -1,4 +0,0 @@
-let addValues = document.getElementById('addNumber');
-let subtractValues = document.getElementById('subtractNumber');
-let multiplyValues = document.getElementById('multiplyNumber');
-let devideValues = document.getElementById('devideNumber');
