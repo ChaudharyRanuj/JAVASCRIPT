d6295d37d3190767877db097ae4cd988cbc0fb2f
RANUJ CHAUDHARY <ranujchoudhary@gmail.com>
2024-03-22 15:09:20 +0530

    hash-table

--- DATA_STRUCTURE_ALGORITHM/pract.js
+++ DATA_STRUCTURE_ALGORITHM/pract.js
@@ -1,51 +1,102 @@
-"use strict";
-// const arr = [1,0,-1,0,-2,2].sort((a,b) => a - b);
-// const n = arr.length;
-// const result = [];
-// for (let i = 0; i < n; i++) {
-//   for (let j = i + 1; j < n; j++) {
-//     for (let k = j + 1; k < n; k++) {
-//       for (let l = k + 1; l < n; l++) {
-//         if (arr[i] + arr[j] + arr[k] + arr[l] === 8) {
-//           insertUniquePairs([arr[i], arr[j], arr[k], arr[l]])
-//         }
-//       }
+// Given an array and an integer K, find the maximum for each and every contiguous subarray of size K.
+
+// let arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let K = 3;
+// let max = Number.NEGATIVE_INFINITY;
+// for (let i = 0; i <= arr.length - K; i++) {
+//   for (let j = i; j < i + K; j++) {
+//    if (max < arr[j]) {
+//       max = arr[j];
 //     }
 //   }
+//   console.log(max);
+// }
+// written as O(N * K)
+// Auxiliary Space: O(1)
+
+// let windowItem = [];
+
+// for (let i = 0; i < K; i++) {
+//   windowItem.push(arr[i]);
 // }
 
-// function insertUniquePairs(arr) {
-//   let status = false;
-//   for (let i = 0; i < result.length; i++) {
-//     status = result[i].every((ele, index) => ele === arr[index]);
-//     if (status === true) {
-//       break;
+// let max = Math.max(...windowItem);
+// let ans = []
+// ans.push(max)
+// for (let i = K; i < arr.length; i++) {
+//   windowItem.shift();
+//   windowItem.push(arr[i]);
+
+//   max = Math.max(...windowItem);
+//   ans.push(max)
+// }
+
+// let arr = [1, 12, -5, -6, 50, 3];
+// let k = 4;
+
+// let sum = 0;
+// for (let i = 0; i < k; i++) {
+//   sum += arr[i];
+// }
+// let avg = sum / k;
+// let maxAvg = avg;
+// for (let i = k; i < arr.length; i++) {
+//   sum = sum - arr[i - k] + arr[i];
+//   avg = sum / k;
+//   if (avg > maxAvg) {
+//     maxAvg = avg;
+//   }
+// }
+// return  maxAvg.toFixed(5)
+
+// time complexity:
+
+// const s = "cbaebabacd";
+// let t = "abc";
+
+// let s = "abab",
+//   t = "ab";
+// let k = t.length;
+// let window_str = "";
+// let result = [];
+// for (let i = 0; i < k; i++) {
+//   window_str += s[i];
+// }
+// let isAnagram = findAnagram(window_str, t);
+// for (let i = k; i < s.length; i++) {
+//   if (i === k) {
+//     if (isAnagram) result.push(i - k);
+//   }
+//   window_str = window_str.slice(1) + s[i];
+//   isAnagram = findAnagram(window_str, t);
+//   if (isAnagram) result.push(i - k + 1);
+// }
+// console.log(result);
+// function findAnagram(s, t) {
+//   if (s.length !== t.length) return false;
+//   let map = new Map();
+//   for (let i = 0; i < s.length; i++) {
+//     if (map.has(s[i])) {
+//       map.set(s[i], map.get(s[i]) + 1);
+//     } else {
+//       map.set(s[i], 1);
 //     }
 //   }
 
-//   if (status === false) {
-//     result.push(arr);
+//   for (let i = 0; i < t.length; i++) {
+//     if (map.has(t[i])) {
+//       map.set(t[i], map.get(t[i]) - 1);
+//     } else {
+//       return false;
+//     }
 //   }
+//   let keys = map.keys();
+//   for (let key of keys) {
+//     if (map.get(key) != 0) {
+//       return false;
+//     }
+//   }
+//   return true;
 // }
+// console.log();
 
-/*
-TIME COMPLEXITY: O(n5)
-SPACE COMPLEXITY: O(n)
-*/
-// const arr = [3,5,6,7]
-// let target = 9
-// for(let i = 0 ;i < arr.length ;i++) {
-//   for(let j = i + 1 ;j < arr.length ;j++) {
-//    const subSeq = arr.slice(i,j)
-//   //  console.log(subSeq);
-//   if(subSeq.length === 1 && subSeq[0] + subSeq[0] <= target) {
-//   console.log(subSeq);
-//   } else if(subSeq[0] + subSeq[subSeq.length - 1] <= target) {
-//     console.log(subSeq);
-//   }
-//   }
-// }
-
-
-x = 20
-var x
