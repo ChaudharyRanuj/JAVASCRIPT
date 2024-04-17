4b24eb5a05d28496706d4ffb6178a2214fd0eff9
RANUJ CHAUDHARY <ranujchoudhary@gmail.com>
2024-04-17 20:15:45 +0530

    stack problems

---
+++ DATA_STRUCTURE_ALGORITHM/STACK/STACK_DSA_PROBLEMS.js
@@ -0,0 +1,193 @@
+// Q1. Remove All Adjacent Duplicates In String (EASY)
+/*
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+Output: false
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+*/
+
+// SOLUTION Q1
+
+// var removeDuplicates = function(s) {
+//   let stack = []
+// for(let i = 0 ; i < s.length; i++) {
+//    if(stack.length === 0) {
+//        stack.push(s[i])
+//    } else {
+//        let stackLastEle =  stack[stack.length - 1]
+//        if(stackLastEle === s[i]) {
+//            stack.pop()
+//        } else {
+//            stack.push(s[i])
+//        }
+//    }
+// }
+
+// return stack.join("");
+// };
+// BIG O
+/*
+time complexity : O(n)
+space complexity : O(n)
+*/
+
+// Q2. Remove All Adjacent Duplicates in String II (MEDIUM)
+/*
+
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+*/
+// SOLUTION Q2
+
+
+
+// Q3. Valid Parentheses (EASY)
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "({[)"
+Output: false
+
+*/
+// let s = "({[)";
+
+// var isValid = function (s) {
+//   let stack = [];
+//   let obj = {
+//     "}": "{",
+//     "]": "[",
+//     ")": "(",
+//   };
+//   for (let i = 0; i < s.length; i++) {
+//     if(stack.length === 0) {
+//         stack.push(s[i])
+//     } else {
+//         if (stack[stack.length - 1] === obj[s[i]]) {
+//             stack.pop();
+//           } else {
+//             stack.push(s[i]);
+//           }
+//     }
+//   }
+// };
+// isValid(s);
+
+// Q4. Remove All Adjacent Duplicates in String II (MEDIUM)
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+*/
+
+var MinStack = function () {
+  this.minStack = [],
+      this.stack = []
+};
+
+/** 
+* @param {number} val
+* @return {void}
+*/
+MinStack.prototype.push = function (val) {
+  // push value to minStack
+  if (this.minStack.length === 0) {
+      this.minStack.push(val)
+  } else {
+      // push value in min stack if val less than top element of min stack
+      let minElement = this.minStack[this.minStack.length - 1]
+      if (val <= minElement) {
+          this.minStack.push(val)
+      }
+  }
+  // push value to stack
+  this.stack.push(val)
+
+};
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function () {
+  let poppedEle = this.stack.pop()
+  // if element removed from stack is equal to min value at top of minstack than pop
+  let minElement = this.minStack[this.minStack.length - 1]
+  if (poppedEle === minElement) {
+      this.minStack.pop()
+  }
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1]
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1]
+};
+
+
--- DATA_STRUCTURE_ALGORITHM/pract.js
+++ DATA_STRUCTURE_ALGORITHM/pract.js
@@ -1,181 +1,63 @@
-// ************** SINGLY LINKED LIST *****************
-// Uses:
+var MinStack = function () {
+  this.minStack = [],
+      this.stack = []
+};
 
-/*
- * Used in React by Fiber Reconsiler to perform task in order of their parent child relationship.
-
-
+/** 
+* @param {number} val
+* @return {void}
 */
-
-// (HEAD) 1 --> 2 --> 3 --> 4 --> 5 (TAIL) --> null
-
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
-
-class SinglyLinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  push(val) {
-    var newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.length++;
-    return this;
-  }
-
-  pop() {
-    if (!this.head) return undefined;
-    var current = this.head;
-    var newTail = current;
-
-    while (current.next) {
-      newTail = current;
-      current = current.next;
-    }
-    this.tail = newTail;
-    this.length--;
-
-    if (this.length === 0) {
-      this.tail = null;
-      this.head = null;
-    }
-
-    return current.val;
-  }
-
-  shift() {
-    if (!this.head) return undefined;
-    var current = this.head;
-    this.head = current.next;
-    this.length--;
-    if (this.length === 0) {
-      this.tail = null;
-    }
-    return current;
-  }
-
-  unshift(val) {
-    var newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    this.length++;
-    return this;
-  }
-
-  get(index) {
-    if (index < 0 || index > this.length) return null;
-    let counter = 0;
-    let current = this.head;
-    while (counter !== index) {
-      current = current.next;
-      counter++;
-    }
-    return current;
-  }
-
-  set(index, val) {
-    if (index < 0 || index > this.length) return null;
-    let foundNode = this.get(index);
-    if (foundNode) {
-      foundNode.val = val;
-      return true;
-    }
-    return false;
-  }
-
-  insert(index, val) {
-    if (index < 0 || index > this.length) return undefined;
-    if (index === 0) return !!this.unshift(val);
-    if (index === this.length) return !!this.push(val);
-    let prevNode = this.get(index - 1);
-    let newNode = new Node(val);
-    let temp = prevNode.next;
-    prevNode.next = newNode;
-    newNode.next = temp;
-    this.length++;
-    return true;
-  }
-
-  remove(index) {
-    if (index < 0 || index > this.length) return undefined;
-    if (index === 0) return !!this.pop();
-    if (index === this.length) return !!this.shift();
-    let prevNode = this.get(index - 1);
-    let removedNode = prevNode.next;
-    prevNode.next = removedNode.next;
-    this.length--;
-    return true;
-  }
-
-  print() {
-    let arr = [];
-    let current = this.head;
-    while (current) {
-      arr.push(current.val);
-      current = current.next;
-    }
-    console.log(arr);
-  }
-
-  // 1 --> 2 --> 3 --> 4 --> null
-  // prev = null, curr, temp
-  // null <--  1   <--  2 --> 3  --> 4
-  // prev     curr     next
-  // take three pointers prev, curr, temp
-
-  // ****** REVERSE LINKED LIST VVVV..IMPORTANT ******
-  reverse() {
-    let cur = this.head;
-    this.head = this.tail;
-    this.tail = cur;
-    let prev = null;
-    let next;
-    while (cur != null) {
-      next = cur.next;
-      cur.next = prev;
-      prev = cur;
-      cur = next;
-    }
-    return this;
-  }
-}
-const newList = new SinglyLinkedList();
-let arr = [1, 1, 2, 3, 3];
-for (let i = 0; i < arr.length; i++) {
-  newList.push(arr[i]);
-}
-
-let head = newList.head;
-// console.log(head);
-let set = new Set();
-let list = head;
-let temp = list;
-let current = head;
-while (current !== null) {
-  if (!set.has(current.val)) {
-    set.add(current.val);
+MinStack.prototype.push = function (val) {
+  // push value to minStack
+  if (this.minStack.length === 0) {
+      this.minStack.push(val)
   } else {
-    temp.next = temp.next.next;
-    temp = temp.next.next;
- 
+      // push value in min stack if val less than top element of min stack
+      let minElement = this.minStack[this.minStack.length - 1]
+      if (val < minElement) {
+          this.minStack.push(val)
+      }
   }
-  current = current.next;
-}
+  // push value to stack
+  this.stack.push(val)
+
+};
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function () {
+  let poppedEle = this.stack.pop()
+  // if element removed from stack is equal to min value at top of minstack than pop
+  let minElement = this.minStack[this.minStack.length - 1]
+  if (poppedEle === minElement) {
+      this.minStack.pop()
+  }
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1]
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1]
+};
+
+
+  
+  let newStack = new MinStack()
+  
+  newStack.push(-2)
+  newStack.push(0)
+  newStack.push(-3)
+  console.log(newStack.getMin());
+newStack.pop();
+newStack.top()  
+console.log(newStack);
+  console.log(newStack.getMin());
