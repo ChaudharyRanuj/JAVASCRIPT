5188037efa3d810c6c7caf1fcea7284d5b0bfefd
RANUJ CHAUDHARY <ranujchoudhary@gmail.com>
2024-04-20 18:27:32 +0530

    image carousel update in code

--- JAVASCRIPT PRACTICE PROJECTS/50_PROJECTS/34_Image_Crousel/index.js
+++ JAVASCRIPT PRACTICE PROJECTS/50_PROJECTS/34_Image_Crousel/index.js
@@ -4,7 +4,7 @@ const pervBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 
 const widthOfImage = 500;
-let imageContainerLength = widthOfImage * images.length;
+let imageContainerLength = images.length;
 let count = 0;
 
 if (count === 0) {
@@ -12,23 +12,23 @@ if (count === 0) {
 }
 
 pervBtn.addEventListener("click", () => {
-  count -= widthOfImage;
-  imageContainer.style.transform = `translateX(-${count}px)`;
+  count--;
+  imageContainer.style.transform = `translateX(-${count * widthOfImage}px)`;
 
   if (count === 0) {
     pervBtn.classList.add("disable");
-  } else if (count < 1500) {
+  } else if (count < imageContainerLength - 1) {
     nextBtn.classList.remove("disable");
   }
 });
 
 nextBtn.addEventListener("click", () => {
-  count += widthOfImage;
-  imageContainer.style.transform = `translateX(-${count}px)`;
+  count++;
+  imageContainer.style.transform = `translateX(-${count * widthOfImage}px)`;
 
-  if (count === 1500) {
+  if (count > imageContainerLength - 2) {
     nextBtn.classList.add("disable");
-  } else if (count === 500) {
+  } else if (count > 0) {
     pervBtn.classList.remove("disable");
   }
 });
