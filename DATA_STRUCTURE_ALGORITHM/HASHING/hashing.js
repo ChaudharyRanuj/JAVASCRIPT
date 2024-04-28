f37bf403c15938af45e62278fe18d82f69bd541b
RANUJ CHAUDHARY <ranujchoudhary@gmail.com>
2024-04-28 16:43:27 +0530

    group anagram and sort array by frequency

--- DATA_STRUCTURE_ALGORITHM/HASHING/hashing.js
+++ DATA_STRUCTURE_ALGORITHM/HASHING/hashing.js
@@ -535,3 +535,56 @@ Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 // }
 
 // return output;
+
+// Q10 Sort Array by Increasing Frequency
+
+// BRUTE FORCE
+// let nums = [2, 2, 2, 3, 1, 1, 1].sort((a, b) => a - b);
+// let map = new Map();
+// for (let ele of nums) {
+//   if (!map.has(ele)) {
+//     map.set(ele, 1);
+//   } else {
+//     map.set(ele, map.get(ele) + 1);
+//   }
+// }
+
+// const arr = Array.from(map).sort((a, b) => a[1] - b[1]);
+
+// const freqMap = new Map();
+
+// for (let pair of arr) {
+//   const [key, value] = pair;
+//   if (freqMap.has(value)) {
+//     let newAr = [];
+//     for (let i = 0; i < value; i++) {
+//       newAr.push(key);
+//     }
+//     freqMap.set(value, [...newAr, ...freqMap.get(value)]);
+//   } else {
+//     let arr = [];
+//     for (let i = 0; i < value; i++) {
+//       arr.push(key);
+//     }
+//     freqMap.set(value, [...arr]);
+//   }
+// }
+
+// const output = [...freqMap].reduce((prev, curr) => [...prev, ...curr[1]], []);
+// return output;
+
+
+// OPTIMISED SOLIUTION
+// let count = {}
+
+// for (let num of nums) {
+//     count[num] = (count[num] || 0) + 1
+// }
+
+// nums.sort((a, b) => {
+//     if (count[a] !== count[b]) {
+//         return count[a] - count[b];
+//     } else {
+//         return b - a;
+//     }
+// });
