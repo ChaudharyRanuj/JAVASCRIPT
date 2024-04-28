d6ac05bc263f03c726e5e3815b4aba5dc862d09a
RANUJ CHAUDHARY <ranujchoudhary@gmail.com>
2024-04-28 13:51:58 +0530

    custom sort and group anagram

--- DATA_STRUCTURE_ALGORITHM/pract.js
+++ DATA_STRUCTURE_ALGORITHM/pract.js
@@ -1,120 +1,21 @@
-// let arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19];
-// let arr2 = [2, 1, 4, 3, 9, 6];
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 
-// let map = new Map();
-// let set = new Set();
-
-// for (let i = 0; i < arr2.length; i++) {
-//   set.add(arr2[i]);
-// }
-
-// let notFound = [];
-// for (let i = 0; i < arr1.length; i++) {
-//   if (map.has(arr1[i])) {
-//     map.set(arr1[i], map.get(arr1[i]) + 1);
-//   } else {
-//     map.set(arr1[i], 1);
-//   }
-
-//   if (!set.has(arr1[i])) {
-//     notFound.push(arr1[i]);
-//   }
-// }
-// console.log(map);
-// let ans = [];
-// for (let i = 0; i < arr2.length; i++) {
-//   if (map.get(arr2[i])) {
-//     console.log(map.get(arr2[i]));
-//    for (let j = 0 ;j < map.get(arr2[i]); j++) {
-//       ans.push(arr2[i]);
-//     }
-//   }
-// }
-// notFound.sort((a,b) => a -b)
-
-// ans = [...ans, ...notFound]
-// return ans;
-
-// // SECOND SOLUTION
-// let arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19, 7, 19, 19, 17, 18].sort(
-//   (a, b) => a - b
-// );
-// let arr2 = [2, 1, 4, 3, 9, 6];
-// let set = new Set();
-
-// for (let ele of arr2) {
-//   set.add(ele);
-// }
-
-// let found = [];
-// let notFound = [];
-// for (let ele of arr1) {
-//   if (set.has(ele)) {
-//     found.push(ele);
-//   } else {
-//     notFound.push(ele);
-//   }
-// }
-
-// let map = new Map();
-
-// for (let ele of found) {
-//   if (map.has(ele)) {
-//     map.set(ele, map.get(ele) + 1);
-//   } else {
-//     map.set(ele, 1);
-//   }
-// }
-
-// let output = [];
-// for (let ele of arr2) {
-//   if (map.has(ele)) {
-//     let end = map.get(ele);
-//     for (let j = 0; j < end; j++) {
-//       output.push(ele);
-//     }
-//   }
-// }
-
-// for (let ele of notFound) {
-//   output.push(ele)
-// }
-
-// return output
-
-// 4th solution
-let arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19, 7, 19, 19, 17, 18].sort(
-  (a, b) => a - b
-);
-let arr2 = [2, 1, 4, 3, 9, 6];
-// create arr1 element count map
+strs.sort();
 let map = new Map();
-// create ordered map as arr is sorted
-for (let ele of arr1) {
-  if (map.has(ele)) {
-    map.set(ele, map.get(ele) + 1);
+
+for (let ele of strs) {
+  let sortedWord = ele.split("").sort().join("");
+
+  if (!map.has(sortedWord)) {
+    map.set(sortedWord, [ele]);
   } else {
-    map.set(ele, 1);
+    map.set(sortedWord, [...map.get(sortedWord), ele]);
   }
 }
 
 let output = [];
-for (let ele of arr2) {
-  if (map.has(ele)) {
-    let end = map.get(ele);
-    for (let i = 0; i < end; i++) {
-      output.push(ele);
-      map.set(ele, map.get(ele) - 1);
-    }
-  }
+for (let [key, value] of map) {
+  output.push(value);
 }
-// push remaining values
-for (let pair of map) {
-  let [key, value] = pair;
-  if (value > 0) {
-    for (let i = 0; i < value; i++) {
-      output.push(key);
-    }
-  }
-}
+
 return output;
