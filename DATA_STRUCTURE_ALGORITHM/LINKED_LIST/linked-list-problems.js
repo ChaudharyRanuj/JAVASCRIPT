39112513a9d5bcc7c7a587de891d11d1dd4dfbe3
RANUJ CHAUDHARY <ranujchoudhary@gmail.com>
2024-04-30 15:07:03 +0530

    identical linked list

--- DATA_STRUCTURE_ALGORITHM/LINKED_LIST/linked-list-problems.js
+++ DATA_STRUCTURE_ALGORITHM/LINKED_LIST/linked-list-problems.js
@@ -1,7 +1,7 @@
 // Q1 Linked List Insertion
 // Basic
 
-// Create a link list of size N according to the given input literals. Each integer input is accompanied by an indicator which can either be 0 or 1. If it is 0, insert the integer in the beginning of the link list. If it is 1, insert the integer at the end of the link list. 
+// Create a link list of size N according to the given input literals. Each integer input is accompanied by an indicator which can either be 0 or 1. If it is 0, insert the integer in the beginning of the link list. If it is 1, insert the integer at the end of the link list.
 // Hint: When inserting at the end, make sure that you handle NULL explicitly.
 
 // Example 1:
@@ -30,3 +30,35 @@
 //       return head;
 //   }
 // }
+
+// Q2: Identical Linked Lists
+/*
+Given two Singly Linked List of N and M nodes respectively. The task is to check whether two linked lists are identical or not. 
+Two Linked Lists are identical when they have same data and with same arrangement too.
+
+Example 1:
+
+Input:
+LinkedList1: 1->2->3->4->5->6
+LinkedList2: 99->59->42->20
+Output: Not identical 
+*/
+// class Solution {
+//   //Function to check whether two linked lists are identical or not.
+//   areIdentical(head1, head2) {
+//     while (head1 !== null) {
+//       if (head1.data != head2.data) {
+//         return false;
+//       }
+//       head1 = head1.next;
+//       if (head1 === null && head2.next) {
+//         return false;
+//       }
+//       head2 = head2.next;
+//     }
+
+//     return true;
+//     //your code here
+//   }
+// }
+
