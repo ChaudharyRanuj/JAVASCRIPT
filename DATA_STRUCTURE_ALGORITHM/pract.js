5c87a81613e069384a2ad8f88bd446bd79544a2c
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-05-22 11:33:32 +0530

    reverse roman to number longest prefix

--- DATA_STRUCTURE_ALGORITHM/pract.js
+++ DATA_STRUCTURE_ALGORITHM/pract.js
@@ -1,12 +1,24 @@
-let arr = [1, 1, 1, 1];
-let k = 2;
-let n = arr.length;
-let count = 0;
-
-for (let i = 0; i < n; i++) {
- let diff = k - arr[i]
- let map = new Map()
-  
- 
+let roman = "DCCLXXXIX";
+let n = roman.length
+let symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+let ans = 0
+for(let i = 0 ; i< n - 1; i++) {
+    if(symbols[roman[i]] >= symbols[roman[i+1]]) {
+        ans += symbols[roman[i]]
+    } else {
+        ans -= symbols[roman[i]]
+    }
 }
-console.log(count);
+
+if(roman.length > 1) {
+    ans += symbols[roman[n - 1]]
+}
+
