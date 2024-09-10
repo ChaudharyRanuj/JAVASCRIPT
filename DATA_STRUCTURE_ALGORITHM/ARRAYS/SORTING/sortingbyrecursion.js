befdaca6d98acc5cda087737d4f639ecfb930361
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-09-10 19:09:03 +0530

    bubble sort and insertion sort by recursion

--- DATA_STRUCTURE_ALGORITHM/ARRAYS/SORTING/sortingbyrecursion.js
+++ DATA_STRUCTURE_ALGORITHM/ARRAYS/SORTING/sortingbyrecursion.js
@@ -1,4 +1,21 @@
+// BUBBLE SORT
 const arr = [1, 5, 6, 7, 4, 3, 2];
+
+function recursionBubbleSort(i, arr) {
+  if (i > arr.length) {
+    return arr;
+  }
+  let j = 0;
+  while (j < arr.length) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+    j++;
+  }
+  return recursionBubbleSort(i + 1, arr);
+}
+
+// INSERTION SORT
 function insertionSort(i, arr) {
   if (i > arr.length - 1) {
     return arr;
